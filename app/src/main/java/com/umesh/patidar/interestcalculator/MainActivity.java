bfba5b085f818d37a40fcb93d4fbe838a8b47350
Umesh <Umeshdangrecha240@gmail.com>
2020-05-23 13:26:22 +0530

    Completed

--- app/src/main/java/com/umesh/patidar/interestcalculator/MainActivity.java
+++ app/src/main/java/com/umesh/patidar/interestcalculator/MainActivity.java
@@ -2,16 +2,21 @@ package com.umesh.patidar.interestcalculator;
 
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +25,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView date1, date2, date, days, amo, interest, totalAmount;
     private Button submit;
     private CheckBox checkBox;
+    private View v1, v2, v3, v4;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,37 +42,104 @@ public class MainActivity extends AppCompatActivity {
         date1.setText(s);
         date2.setText(s);
 
+        submit.setOnClickListener(v -> submit());
+
 
         date1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, AlertDialog.THEME_HOLO_LIGHT, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        String s = String.format("%02d", dayOfMonth) + "/" + String.format("%02d", 1 + month) + "/" + year;
-                        date1.setText(s);
-                    }
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, AlertDialog.THEME_HOLO_LIGHT, (view, year1, month1, dayOfMonth) -> {
+                    String s1 = String.format("%02d", dayOfMonth) + "/" + String.format("%02d", 1 + month1) + "/" + year1;
+                    date1.setText(s1);
                 }, year, month, day);
                 datePickerDialog.show();
                 datePickerDialog.getDatePicker().setMaxDate(Calendar.getInstance().getTimeInMillis());
-                Calendar cal = Calendar.getInstance();
             }
         });
         date2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, AlertDialog.THEME_HOLO_LIGHT, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        String s = String.format("%02d", dayOfMonth) + "/" + String.format("%02d", 1 + month) + "/" + year;
-                        date1.setText(s);
-                    }
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, AlertDialog.THEME_HOLO_LIGHT, (view, year12, month12, dayOfMonth) -> {
+                    String s12 = String.format("%02d", dayOfMonth) + "/" + String.format("%02d", 1 + month12) + "/" + year12;
+                    date2.setText(s12);
                 }, year, month, day);
                 datePickerDialog.show();
                 datePickerDialog.getDatePicker().setMaxDate(Calendar.getInstance().getTimeInMillis());
-                Calendar cal = Calendar.getInstance();
             }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void submit() {
+        String d1 = date1.getText().toString();
+        String d2 = date2.getText().toString();
+        String[] d1string = d1.split("/");
+        String[] d2string = d2.split("/");
+
+        LocalDate dd1 = LocalDate.parse(d1string[2] + '-' + d1string[1] + '-' + d1string[0]);
+        LocalDate dd2 = LocalDate.parse(d2string[2] + '-' + d2string[1] + '-' + d2string[0]);
+        Period period = Period.between(dd1, dd2);
+        int d = period.getDays();
+        int m = period.getMonths();
+        int y = period.getYears();
+        if (d < 0)
+            d *= -1;
+        if (m < 0)
+            m *= -1;
+        if (y < 0)
+            y *= -1;
+        String amountString = amount.getText().toString();
+        String rateString = rate.getText().toString();
+        int amountValue = 0;
+        int rateValue = 0;
+
+        if (!amountString.isEmpty()) {
+            amountValue = Integer.parseInt(amountString);
+        }
+        if (!rateString.isEmpty()) {
+            rateValue = Integer.parseInt(rateString);
+        }
+
+        int interestValue = 0;
+
+        if (checkBox.isChecked()) {
+
+            int demoAmount = amountValue;
+            if (y > 0) {
+                while (y-- > 0) {
+                    demoAmount += 12 * (demoAmount * rateValue / 100);
+                }
+            }
+            int interestPerMonth = demoAmount * rateValue / 100;
+            int interestPerDay = interestPerMonth / 30;
+            interestValue = m * interestPerMonth + d * interestPerDay;
+            demoAmount += interestValue;
+            interestValue = demoAmount - amountValue;
+
+            date.setText("दिनांक " + d1 + " से " + d2 + " तक");
+            days.setText("कुल " + y + " साल " + m + " महीने " + d + " दिन");
+            amo.setText("मूल : " + amountValue);
+            interest.setText("कुल ब्याज : " + interestValue);
+            totalAmount.setText("कुल रूपए : " + (amountValue + interestValue));
+
+
+        } else {
+            int interestPerMonth = amountValue * rateValue / 100;
+            int interestPerDay = interestPerMonth / 30;
+            interestValue = (y * 12 + m) * interestPerMonth + d * interestPerDay;
+
+            date.setText("दिनांक " + d1 + " से " + d2 + " तक");
+            days.setText("कुल " + y + " साल " + m + " महीने " + d + " दिन");
+            amo.setText("मूल : " + amountValue);
+            interest.setText("कुल ब्याज : " + interestValue);
+            totalAmount.setText("कुल रूपए : " + (amountValue + interestValue));
+        }
+        v1.setBackgroundColor(Color.rgb(237, 0, 0));
+        v2.setBackgroundColor(Color.rgb(237, 0, 0));
+        v3.setBackgroundColor(Color.rgb(237, 0, 0));
+        v4.setBackgroundColor(Color.rgb(237, 0, 0));
+
+
     }
 
     private void initialize() {
@@ -83,5 +157,15 @@ public class MainActivity extends AppCompatActivity {
 
         submit = findViewById(R.id.submit);
         checkBox = findViewById(R.id.checkbox);
+
+        v1 = findViewById(R.id.view1);
+        v2 = findViewById(R.id.view2);
+        v3 = findViewById(R.id.view3);
+        v4 = findViewById(R.id.view4);
+    }
+
+    public void showMyBio(View view) {
+        Intent intent = new Intent(this, MyDetail.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/umesh/patidar/interestcalculator/MyDetail.java
@@ -0,0 +1,14 @@
+package com.umesh.patidar.interestcalculator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_detail);
+    }
+}
